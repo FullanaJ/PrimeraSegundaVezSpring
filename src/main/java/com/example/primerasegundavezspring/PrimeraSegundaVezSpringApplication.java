7d8691f2abc13d6971fd88211fdb0324cc5e7739
Jorge Fullana Estela <jorgefullana3@gmail.com>
2023-12-14 19:42:07 +0100

    Initial commit

---
+++ src/main/java/com/example/primerasegundavezspring/PrimeraSegundaVezSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.primerasegundavezspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeraSegundaVezSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimeraSegundaVezSpringApplication.class, args);
+    }
+
+}
